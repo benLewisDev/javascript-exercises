e4f80ec4c73a9e046d0c3582256d5c7ca04e297a
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-09 21:22:59 +0930

    complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let stringReversed = input.split("").reverse().join("");
+  return stringReversed;
 };
+
+// reverseString("hello World");
 
 // Do not edit below this line
 module.exports = reverseString;
