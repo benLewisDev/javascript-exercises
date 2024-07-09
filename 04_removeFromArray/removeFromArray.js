3a5c3719c6ddb2f4d23b2cb69c1a46ed522fb055
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-09 22:35:01 +0930

    complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...arg) {
+  let newArray = [];
+  array.forEach((item) => {
+    if (!arg.includes(item)) newArray.push(item);
+  });
+  return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
