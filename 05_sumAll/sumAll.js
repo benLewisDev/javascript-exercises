1c8e2c724893a45cf31c34a51345a7f953a54f42
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-11 21:00:22 +0930

    Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  if (typeof numOne != "number" || typeof numTwo != "number") return "ERROR";
+  if (numOne < 0 || numTwo < 0) return "ERROR";
 
+  let sortedNums = [numOne, numTwo].sort(function (a, b) {
+    return a - b;
+  });
+
+  let newNum = 0;
+  for (i = sortedNums[0]; i <= sortedNums[1]; i++) {
+    newNum += i;
+  }
+  return newNum;
 };
+
+sumAll(-10, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
