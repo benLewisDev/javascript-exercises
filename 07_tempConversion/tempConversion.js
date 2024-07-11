39439dff95390713597cb570b304d75b171a1760
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-11 22:09:12 +0930

    Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = ((tempFahrenheit - 32) / 1.8).toFixed(1);
+  let converted = parseFloat(tempCelsius)
+  return converted
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = ((tempCelsius * 1.8) + 32).toFixed(1);
+  let converted = parseFloat(tempFahrenheit)
+  return converted
 };
+
+console.log(convertToFahrenheit(73.2))
 
 // Do not edit below this line
 module.exports = {
